e012ebd3d52ad4c2af80d430a27c37fccec0e6eb
medusalix <ss7@live.de>
2021-07-07 12:49:10 +0200

    Add racing wheel button input

--- driver/wheel.c
+++ driver/wheel.c
@@ -8,6 +8,25 @@
 
 #include "common.h"
 
+enum gip_wheel_button {
+	GIP_WL_BTN_MENU = BIT(2),
+	GIP_WL_BTN_VIEW = BIT(3),
+	GIP_WL_BTN_A = BIT(4),
+	GIP_WL_BTN_B = BIT(5),
+	GIP_WL_BTN_X = BIT(6),
+	GIP_WL_BTN_Y = BIT(7),
+	GIP_WL_BTN_DPAD_U = BIT(8),
+	GIP_WL_BTN_DPAD_D = BIT(9),
+	GIP_WL_BTN_DPAD_L = BIT(10),
+	GIP_WL_BTN_DPAD_R = BIT(11),
+	GIP_WL_BTN_BUMPER_L = BIT(12),
+	GIP_WL_BTN_BUMPER_R = BIT(13),
+};
+
+struct gip_wheel_pkt_input {
+	__le16 buttons;
+} __packed;
+
 struct gip_wheel {
 	struct gip_common common;
 };
@@ -19,6 +38,16 @@ static int gip_wheel_init_input(struct gip_wheel *wheel)
 	int err;
 
 	input_set_capability(dev, EV_KEY, BTN_MODE);
+	input_set_capability(dev, EV_KEY, BTN_START);
+	input_set_capability(dev, EV_KEY, BTN_SELECT);
+	input_set_capability(dev, EV_KEY, BTN_A);
+	input_set_capability(dev, EV_KEY, BTN_B);
+	input_set_capability(dev, EV_KEY, BTN_X);
+	input_set_capability(dev, EV_KEY, BTN_Y);
+	input_set_capability(dev, EV_KEY, BTN_TL);
+	input_set_capability(dev, EV_KEY, BTN_TR);
+	input_set_abs_params(dev, ABS_HAT0X, -1, 1, 0, 0);
+	input_set_abs_params(dev, ABS_HAT0Y, -1, 1, 0, 0);
 
 	err = input_register_device(dev);
 	if (err)
@@ -41,8 +70,30 @@ static int gip_wheel_op_guide_button(struct gip_client *client, bool pressed)
 
 static int gip_wheel_op_input(struct gip_client *client, void *data, int len)
 {
+	struct gip_wheel *wheel = dev_get_drvdata(&client->dev);
+	struct input_dev *dev = wheel->common.input_dev;
+	struct gip_wheel_pkt_input *pkt = data;
+	u16 buttons = le16_to_cpu(pkt->buttons);
+
+	if (len < sizeof(*pkt))
+		return 0;
+
 	print_hex_dump_debug("wheel input: ", DUMP_PREFIX_NONE, 16, 1,
 				data, len, false);
+
+	input_report_key(dev, BTN_START, buttons & GIP_WL_BTN_MENU);
+	input_report_key(dev, BTN_SELECT, buttons & GIP_WL_BTN_VIEW);
+	input_report_key(dev, BTN_A, buttons & GIP_WL_BTN_A);
+	input_report_key(dev, BTN_B, buttons & GIP_WL_BTN_B);
+	input_report_key(dev, BTN_X, buttons & GIP_WL_BTN_X);
+	input_report_key(dev, BTN_Y, buttons & GIP_WL_BTN_Y);
+	input_report_key(dev, BTN_TL, buttons & GIP_WL_BTN_BUMPER_L);
+	input_report_key(dev, BTN_TR, buttons & GIP_WL_BTN_BUMPER_R);
+	input_report_abs(dev, ABS_HAT0X,
+			!!(buttons & GIP_WL_BTN_DPAD_R) - !!(buttons & GIP_WL_BTN_DPAD_L));
+	input_report_abs(dev, ABS_HAT0Y,
+			!!(buttons & GIP_WL_BTN_DPAD_D) - !!(buttons & GIP_WL_BTN_DPAD_U));
+	input_sync(dev);
 
 	return 0;
 }
